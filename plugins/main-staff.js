19a42e6f147134ee05278065f46d913bce25a065
MRSHAD0W666 <srperruno13@gmail.com>
2025-03-20 10:13:34 -0800

    Update main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -1,4 +1,4 @@
-let handler = async 
+let handler = async (m, { conn, command, usedPrefix }) => {
 let staff = `✨ *EQUIPO DE AYUDANTES*
 🤖 *Bot:* ${global.botname}
 🌟 *Versión:* ${global.vs}
@@ -24,7 +24,7 @@ let staff = `✨ *EQUIPO DE AYUDANTES*
 🦁 *Rol:* Developer
 📱 *Número:* Wa.me/50558124470
 `
-await conn.sendFile(m.chat, icons, 'yaemori.jpg', staff.trim(), fkontak, true, {
+await conn.sendFile(m.chat, icons, 'https://qu.ax/mUdsk.jpg', staff.trim(), fkontak, true, {
 contextInfo: {
 'forwardingScore': 200,
 'isForwarded': false,
