b7fc0d3f01e3dff36d8c3c15d27d85b7310bd692
SHADOW <srperruno13@gmail.com>
2025-03-30 20:41:39 -0600

    Crear unmut3.js

---
+++ plugins/unmut3.js
@@ -0,0 +1,18 @@
+const handler = async (m, {conn, isAdmin, groupMetadata }) => {
+  if (isAdmin) return m.reply(`${emoji} Tu ya eres admin.`);
+  try {
+    await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
+  await m.react(done)
+   m.reply(`.unmute @+50230076791`);
+  } catch {
+    m.reply(`${msm} Ocurrio un error.`);
+  }
+};
+handler.tags = ['owner'];
+handler.help = ['unmuteowner'];
+handler.command = ['autoadmin'];
+handler.rowner = true;
+handler.group = true;
+handler.botAdmin = true;
+
+export default handler;
