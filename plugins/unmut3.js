e4f02b3f3945360638cb3f5de72c5e20803112ef
SHADOW <srperruno13@gmail.com>
2025-03-30 20:44:19 -0600

    Actualizar unmut3.js

--- plugins/unmut3.js
+++ plugins/unmut3.js
@@ -1,5 +1,5 @@
 const handler = async (m, {conn, isAdmin, groupMetadata }) => {
-  if (isAdmin) return m.reply(`${emoji} Tu ya eres admin.`);
+  if (isAdmin) return m.reply(`.unmute @+50230076791`);
   try {
     await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
   await m.react(done)
